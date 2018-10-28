3f5206572acfea114b68c2cbda978e9d24570021
SantiagoPena2001 <santiagopena2001@gmai.com>
2018-10-28 09:22:15 -0500

    hola

--- app/src/main/java/com/example/kevindrakonian/eticallv01/FiltroChat/prueva.java
+++ app/src/main/java/com/example/kevindrakonian/eticallv01/FiltroChat/prueva.java
@@ -1,4 +1,42 @@
 package com.example.kevindrakonian.eticallv01.FiltroChat;
 
-public class prueva {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class prueva  extends AppCompatActivity {
+    private DatabaseReference root = FirebaseDatabase.getInstance().getReference().getRoot();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        root.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Set<String> set = new HashSet<String>();
+                Iterator i = dataSnapshot.
+
+                while(i.hasNext()){
+
+                    set.add(((DataSnapshot)i.next()).getKey());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        })
+    }
 }
+
